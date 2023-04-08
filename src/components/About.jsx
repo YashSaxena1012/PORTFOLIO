f50bc2d1e90e1928f4c06e84d0efeed4ce15c551
YASH SAXENA <94756047+YashSaxena1012@users.noreply.github.com>
2023-04-08 14:04:29 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,8 +7,13 @@ const About = () => {
             <div className='pb-8 '>
                 <p className='font-bold text-5xl border-b-4 border-black w-fit'>ABOUT</p>
             </div>
-            <p className='hover:animate-pulse font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptatibus voluptates architecto obcaecati doloribus, quibusdam dicta facilis asperiores ratione quas eum voluptatem et sunt illum iure, tempora modi! Tenetur nemo sapiente possimus laudantium sint sed nostrum cumque soluta atque quod nulla, maiores repudiandae dicta autem harum veniam molestiae tempore labore!</p><br />
-            <p className='hover:animate-pulse' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum esse consectetur doloremque officia qui dolorem ad odit veritatis eaque possimus quod explicabo, ullam praesentium enim at ducimus adipisci libero officiis. Eveniet odit est laborum consequuntur excepturi iusto ullam, officiis, non unde amet minus dolores autem quo ipsa mollitia! Rerum, aut?</p>
+            <p className='hover:animate-pulse font-medium'>ash Saxena and I am currently pursuing my BTech from JSS Academy of Technical Education in the field of Information Technology.
+Web development has always gained my attention.I like to build and learn and believe only with practice one can be confident in this field.
+</p><br />
+            <p className='hover:animate-pulse' > My expertise in HTML, CSS, and JavaScript, as well as front-end frameworks like React and Tailwind, allows me to design intuitive and visually appealing user experiences. I prioritize communication and collaboration with clients to deliver products that exceed their expectations and meet their users' needs.</p>
+    <p className='font-2xl'>
+    Let's work together to build something great!
+    </p>
         </div>
       </div>
   )
