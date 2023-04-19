dd0ba84f541cbe6404764dd6640c674543fc4657
YASH SAXENA <94756047+YashSaxena1012@users.noreply.github.com>
2023-04-19 19:38:56 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
             <div className='pb-8 '>
                 <p className='font-bold text-5xl border-b-4 border-black w-fit'>ABOUT</p>
             </div>
-            <p className='hover:animate-pulse font-medium'>ash Saxena and I am currently pursuing my BTech from JSS Academy of Technical Education in the field of Information Technology.
+            <p className='hover:animate-pulse font-medium'>Yash Saxena and I am currently pursuing my BTech from JSS Academy of Technical Education in the field of Information Technology.
 Web development has always gained my attention.I like to build and learn and believe only with practice one can be confident in this field.
 </p><br />
             <p className='hover:animate-pulse' > My expertise in HTML, CSS, and JavaScript, as well as front-end frameworks like React and Tailwind, allows me to design intuitive and visually appealing user experiences. I prioritize communication and collaboration with clients to deliver products that exceed their expectations and meet their users' needs.</p>
